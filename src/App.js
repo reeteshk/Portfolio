44eaab7fed528b36847e6a2879b1974d0683104a
reeteshkesarwani <kesarwani.r@northeastern.edu>
2023-05-02 22:07:29 -0400

    adding basic portfolio

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+
+
+function App() {
+  return (
+    <div>
+      {/* <h1 className="text-4xl font-bold font-signature">Hello World</h1> */}
+      <NavBar/>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HeroImage from '../assets/heroImage.jpg'
+const Home = () => {
+  return (
+    <div>
+      <img src={HeroImage} alt="heroImage" width="100%" height="100%"/>
+    </div>
+  )
+}
+
+export default Home;
