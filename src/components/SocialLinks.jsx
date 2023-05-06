6106b49320b3ed4aa4e598bd604ba34db4896330
reeteshkesarwani <kesarwani.r@northeastern.edu>
2023-05-06 14:00:57 -0400

    fixing resume download

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -56,7 +56,13 @@ function SocialLinks() {
         {links.map(({id,child,href,style,download})=>(
             
             <li key={id} className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500'+" "+style}>
-            <a href={href} className='flex justify-between items-center w-full text-white' download={download} rel='noreferrer' target='_blank'>
+             <a
+              href={href}
+              className="flex justify-between items-center w-full text-white"
+              download={download}
+              target="_blank"
+              rel="noreferrer"
+            >
            {child}</a></li>
         ))
         }
