2fd30f0d912524c7c464bee932e7a8cfa4c4d4d8
reeteshkesarwani <kesarwani.r@northeastern.edu>
2023-05-06 16:07:27 -0400

    adding resume button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,10 @@ import {MdOutlineKeyboardArrowRight} from 'react-icons/md'
 import { Link } from "react-scroll";
 
 const Home = () => {
+
+    const handleDemoUrl = () => {
+        window.open("https://drive.google.com/file/d/1DoFkiAhpQxWpaWt9iOJs-brPZx6ARWjR/view", '_blank')
+    }
   return (
     <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800" >
      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
@@ -27,6 +31,12 @@ const Home = () => {
                 <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
             </Link>
+           <span>
+           <button onClick={() => handleDemoUrl()} class="bg-gradient-to-r  from-cyan-500 to-blue-500 cursor-pointer hover:text-black text-white  py-2 px-4 rounded inline-flex items-center">
+  <svg class="fill-current w-9 h-6 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+  <span>Resume</span>
+</button>
+            </span>
           </div>
           
         </div>
